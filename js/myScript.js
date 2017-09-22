ff7f1ec85109a0b99072feccbcb05b76ad51add1
Jopee Zapido <jopeezapido@users.noreply.github.com>
2017-09-23 00:04:01 +0800

    start google maps api

--- js/myScript.js
+++ js/myScript.js
@@ -14,8 +14,26 @@ $(document).ready(function({
     /*about*/
     //google map APIs
 
+    function initMap() {
+        var prov = {
+            lat: ,
+            lng: 
+        }// add geolocation
 
-    /*Back to top*/
+        //customize features of the map
+        var map = new google.maps.Map(document.getElementById('map'), {
+            zoom: 4,
+            center: prov
+        });
+
+        //input and postion the marker
+        var marker = new.google.maps.Marker({
+            position: prov,
+            map: map
+        });
+    }
+
+    /*Back to top button*/
 
 
     /*Page Translation*/
