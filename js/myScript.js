5ff25a672e909a6c7c49231dd2dc326b25c8c29d
Jopee Zapido <jopeezapido@users.noreply.github.com>
2017-09-26 15:46:38 +0800

    add functionality to responsive toggle navigation

--- js/myScript.js
+++ js/myScript.js
@@ -1,36 +1,8 @@
-$(document).ready(function({
-    /*nav*/
-    //active highlights for anchors
-        
-    //transluscent navigation when scrolling down
+$(document).ready(function() {
 
-    /*about - google maps 
-
-    function initMap() {
-        var prov = {lat: 14.633873, lng: 121.088094};
-
-        //properties of the map
-        var mapProp = {
-            zoom: 4,
-            center: prov
-        };
-
-        //customize features of the map
-        var map = new google.maps.Map(document.getElementById('map'), mapProp);
-
-        //input and postion the marker
-        var marker = new google.maps.Marker({
-            position: prov,
-            map: map
-        });
-    }
-
-    */
-
-    /*Back to top button*/
-
-    /*Page Translation*/
-
+    //responsive toggle
+    $('.navbar-toggler').click(function() {
+        $('.collapse').slideToggle();
+    });
+    
 });
-
-alert('test')
