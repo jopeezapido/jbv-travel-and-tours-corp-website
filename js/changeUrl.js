7d8da29e20c351e9d2416bf4947d7fb7ff1b4731
Jopee Zapido <jopeezapido@users.noreply.github.com>
2017-09-29 16:14:48 +0800

    test url

--- js/changeUrl.js
+++ js/changeUrl.js
@@ -1,15 +1,10 @@
 //function that changes url when in home page
 
 $(document).ready(function() {
-    function loadHome() {
-        history.pushState({}, "Home", "");
-    }
-    loadHome();
+    $('#home').click.(function() {
+        function loadHome() {
+            history.pushState({}, "Home", "");
+        }
+        loadHome();
+    })
 });
-
-$('#home').click.(function() {
-    function loadHome() {
-        history.pushState({}, "Home", "");
-    }
-    loadHome();
-})
