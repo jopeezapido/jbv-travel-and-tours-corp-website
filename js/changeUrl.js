a4db942bfdc249f88d15a7b7f6e6ee48e87b8237
Jopee Zapido <jopeezapido@users.noreply.github.com>
2017-09-29 16:30:20 +0800

    included back and forward

--- js/changeUrl.js
+++ js/changeUrl.js
@@ -1,10 +1,10 @@
 //function that changes url when in home page
 
 $(document).ready(function() {
-    $('#home').click.(function() {
-        function loadHome() {
-            history.pushState({}, "Home", "");
-        }
-        loadHome();
-    })
+    function loadHome() {
+        window.history.pushState({}, "Home", "");
+        window.history.back({}, "Home", "");
+        window.history.forward({}, "Home", "");
+    }
+    loadHome();
 });
