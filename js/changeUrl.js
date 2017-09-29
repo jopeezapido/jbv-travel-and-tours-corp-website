ecdc60961fc4e19dbd74f40df981fe1b9e8151bb
Jopee Zapido <jopeezapido@users.noreply.github.com>
2017-09-29 16:52:15 +0800

    add onload and onclick event to home nav

--- js/changeUrl.js
+++ js/changeUrl.js
@@ -1,10 +1,8 @@
 //function that changes url when in home page
-
-$(document).ready(function() {
     function loadHome() {
         window.history.pushState({}, "Home", "");
         window.history.back({}, "Home", "");
         window.history.forward({}, "Home", "");
     }
-    loadHome();
-});
+
+    
