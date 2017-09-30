0cb94ebfa25973956dd76ea67bd5b3522867803c
Jopee Zapido <jopeezapido@users.noreply.github.com>
2017-09-30 14:13:48 +0800

    restarted URL script

--- js/changeUrl.js
+++ js/changeUrl.js
@@ -1,15 +1,2 @@
 //function that changes url when in home page
 
-var parent = document.querySelector("nav");
-
-parent.addEventListener("popstate", loadUrl);
-
-function loadUrl(e) {
-    if (e.target !== e.currentTarget) {
-        var clickedItem = e.target.id;
-        var url = "/" + clickedItem;
-        history.pushState(clickedItem, null, url);
-        //alert(clickedItem);
-    }
-    e.stopPropagation();
-}
