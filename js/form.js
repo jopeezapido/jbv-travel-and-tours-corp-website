9d2f4886f0d46bfa6f09903829a6d2805d10e421
Jopee Zapido <jopeezapido@users.noreply.github.com>
2017-09-30 23:52:07 +0800

    modify form functionality

--- js/form.js
+++ js/form.js
@@ -1,41 +1,30 @@
 $(document).ready(function() {
-    //test everything once submit button is clicked
     $('#submit').click(function() {
-        var name, phone, email, msg;
-        var crtName, crtPhone, crtEmail, crtMsg;
-        
-        //retrieves value
-        name = document.getElementById("name").value;
-        phone = document.getElementById("tel").value;
-        email = document.getElementById("email").value;
-        msg = document.getElementById("msg").value;
-
-        //set criteria
-        crtName = /([a-z])/ig;
-        crtPhone = /([()+0-9])/g;
-        crtEmail = /^\w+[\w-\.]*\@\w+((-\w+)|(\w*))\.[a-z]{2,3}$/;
-
-        //debug or test
-        //alert(name +"\n" + phone +"\n" + email +"\n" + msg);
-
-        //decision flow
-        if(crtName.test(name)) {
-            if(confirm == "YES") {
-                //tell the sender that email was sent
-                var confirm = prompt("Type YES to confirm sending");
-                //some code that submits a link to a specified email recipient
-
-                alert('Email Sent!\nKindly wait for a response within 24 hours via email or phone.');
-
-                //code that refreshes the page to go back to the top
-                
-            } else {
-                prompt('Try again. Click OK to continue or click CANCEL to review again your inputs.')
-            }
-            
-        } else {
-            alert('Review your inputs.\nKindly ensure you typed the right format.')
-        }
+      var name, contact, email, msg;
+      var testName, testContact, testEmail;
+      
+      //retrieve what values are written in the form inputs
+      name = document.getElementById("name").value;
+      contact = document.getElementById("tel").value;
+      email = document.getElementById("email").value;
+      msg = document.getElementById("message").value;
+      
+      //criteria for testing values
+      testName = /([a-z])/ig.test(name);
+      testContact = /([()+0-9])/g.test(contact);
+      testEmail = /^\w+[\w-\.]*\@\w+((-\w+)|(\w*))\.[a-z]{2,3}$/.test(email);
+   
+      switch(false) {
+        case testName:
+          alert('Check format!');
+        case testContact: 
+          alert('Check format!');
+        case testEmail:
+          alert('Check format!');
+        default:
+          alert('Sent!');
+      } 
+      
     });
-})
+  });
 
