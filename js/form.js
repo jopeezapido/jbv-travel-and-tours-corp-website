59c77cfe8d15d53a2b75aa68e13a6747049a4d13
Jopee Zapido <jopeezapido@users.noreply.github.com>
2017-10-02 21:33:48 +0800

    add criteria for form message textarea

--- js/form.js
+++ js/form.js
@@ -1,30 +1,34 @@
 $(document).ready(function() {
-    $('#submit').click(function() {
-      var name, contact, email, msg;
-      var testName, testContact, testEmail;
-      
-      //retrieve what values are written in the form inputs
-      name = document.getElementById("name").value;
-      contact = document.getElementById("tel").value;
-      email = document.getElementById("email").value;
-      msg = document.getElementById("message").value;
-      
-      //criteria for testing values
-      testName = /([a-z])/ig.test(name);
-      testContact = /([()+0-9])/g.test(contact);
-      testEmail = /^\w+[\w-\.]*\@\w+((-\w+)|(\w*))\.[a-z]{2,3}$/.test(email);
-   
-      switch(false) {
-        case testName:
-          alert('Check format!');
-        case testContact: 
-          alert('Check format!');
-        case testEmail:
-          alert('Check format!');
-        default:
-          alert('Sent!');
-      } 
-      
-    });
+  $('#submit').click(function() {
+    var name, contact, email, msg;
+    var testName, testContact, testEmail, testMsg;
+    
+    //retrieve what values are written in the form inputs
+    name = document.getElementById("name").value;
+    contact = document.getElementById("tel").value;
+    email = document.getElementById("email").value;
+    msg = document.getElementById("message").value;
+    
+    //criteria for testing values
+    testName = /([a-z])/ig.test(name);
+    testContact = /([()+0-9])/g.test(contact);
+    testEmail = /^\w+[\w-\.]*\@\w+((-\w+)|(\w*))\.[a-z]{2,3}$/.test(email);
+    testMsg = /(\w\d\s)/g.test(msg);
+ 
+    switch(false) {
+      case testName:
+        alert('Check format!');
+      case testContact: 
+        alert('Check format!');
+      case testEmail:
+        alert('Check format!');
+      case testMsg:
+        alert('Input a message');
+        break;
+      default:
+        alert('Sent!');
+    } 
+    
   });
+});
 
