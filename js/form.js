446880a9e5ee2042f01e2c65455f8f2fa0d67d96
Jopee Zapido <jopeezapido@users.noreply.github.com>
2017-10-03 23:07:25 +0800

    change js form validation to HTML5/bootstrap constraint validation

--- js/form.js
+++ js/form.js
@@ -1,30 +1,9 @@
 $(document).ready(function() {
-  $('#submit').click(function() {
-    var name, contact, email, msg;
-    var testName, testContact, testEmail, testMsg;
-    
-    //retrieve what values are written in the form inputs
-    name = document.getElementById("name").value;
-    contact = document.getElementById("tel").value;
-    email = document.getElementById("email").value;
-    msg = document.getElementById("message").value;
-    
-    //criteria for testing values
-    testName = /([a-z])/ig.test(name);
-    testContact = /([()+0-9])/g.test(contact);
-    testEmail = /^\w+[\w-\.]*\@\w+((-\w+)|(\w*))\.[a-z]{2,3}$/.test(email);
- 
-    switch(false) {
-      case testName:
-        alert('Check format!');
-      case testContact: 
-        alert('Check format!');
-      case testEmail:
-        alert('Check format!');
-      default:
-        alert('Sent!');
-    } 
-    
-  });
+  $('#submit').validator().click(function(event) {
+    if(event.isDefaultPrevented()){
+      alert('Check format if correct')
+    } else {
+      alert('Sent');
+    }
+  })
 });
-
